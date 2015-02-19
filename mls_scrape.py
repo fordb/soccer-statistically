9d1a6d164cdfba5bd687c05548fa8616d2df6624
Ford Bohrmann <ford@simulmedia.com>
2015-02-19 02:11:08 -0500

    scrapes all player data and populates MySQL database

--- mls_scrape.py
+++ mls_scrape.py
@@ -1,4 +1,9 @@
+import MySQLdb
+import sys
+from unidecode import unidecode
 
+db = MySQLdb.connect(host="localhost", user="root", db="soccer_stat")
+cur = db.cursor()
 
 # inactive players
 url = 'http://www.mlssoccer.com/players?field_player_club_nid=All&tid_2=198&title='
@@ -17,18 +22,34 @@ from bs4 import BeautifulSoup
 import re
 import urllib2
 import datetime
-import sys
+
+proxy_support = urllib2.ProxyHandler({})
+opener = urllib2.build_opener(proxy_support)
 
 # get player data
-f = open('players.csv', 'w')
-f.write('number,position,name,club,age,height,weight,country,active,twitter\n')
+cur.execute('drop table if exists players;')
+create_table = """
+    create table players (
+        player_id int not null auto_increment primary key,
+        number smallint,
+        position varchar(5),
+        name varchar(30),
+        club varchar(25),
+        age smallint,
+        height varchar(5),
+        weight smallint,
+        country varchar(50),
+        active varchar(10),
+        twitter varchar(30));
+"""
+cur.execute(create_table)
 
 # active players only
 for n in range(11):
     print >>sys.stderr, '[{time}] Running page {n}...'.format(time=datetime.datetime.now(), n=n)
     url = 'http://www.mlssoccer.com/players?page={num}&field_player_club_nid=All&tid_2=197&title='.format(num=n)
 
-    page = urllib2.urlopen(url)
+    page = opener.open(url).read()
     soup = BeautifulSoup(page)
 
     numbers = soup.findAll('td', {'class': 'views-field views-field-field-player-jersey-no-value'})
@@ -45,29 +66,62 @@ for n in range(11):
     assert len(numbers) == len(positions) == len(names) == len(clubs) == len(ages) == len(heights) == len(weights) == len(countries) == len(actives) == len(twitters)
 
     for c in range(len(countries)):
-        number = str(numbers[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        position = str(positions[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        name = str(names[c]).split('</a>')[0].split('>')[-1].lstrip().rstrip()
-        club = str(clubs[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        age = str(ages[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        height = str(heights[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        weight = str(weights[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        country = str(countries[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        active = str(actives[c]).split('>')[1].split('<')[0].lstrip().rstrip()
+        number = unidecode(unicode(str(numbers[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if number == '':
+            number = 'Null'
+        position = unidecode(unicode(str(positions[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if position == '':
+            position = 'Null'
+        name = unidecode(unicode(str(names[c]).split('</a>')[0].split('>')[-1].lstrip().rstrip(), "utf-8"))
+        name = name.replace("'", "")
+        if name == '':
+            name = 'Null'
+        club = unidecode(unicode(str(clubs[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if club == '':
+            club = 'Null'
+        age = unidecode(unicode(str(ages[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if age == '':
+            age = 'Null'
+        height = unidecode(unicode(str(heights[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        height = height[:-3].replace("'", "''").replace("\\", "")
+        if height == '':
+            height = 'Null'
+        weight = unidecode(unicode(str(weights[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if weight == '':
+            weight = 'Null'
+        country = unidecode(unicode(str(countries[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if country == '':
+            country = 'Null'
+        active = unidecode(unicode(str(actives[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
         try:
             twitter = str(twitters[c]).split('twitter.com/')[1].split('" target')[0].lstrip().rstrip()
         except IndexError:
             twitter = 'No Twitter'
 
-        f.write(number + ',' + position + ',' + name + ',' + club + ',' + age + ',' + height + ',' + weight + ',' + country + ',' + active + ',' + twitter + '\n')
+        insert_data = """
+            insert into players
+                (number, position, name, club, age, height, weight, country, active, twitter)
+                values (
+                    {num},
+                    '{position}',
+                    '{name}',
+                    '{club}',
+                    {age},
+                    '{height}',
+                    {weight},
+                    '{country}',
+                    '{active}',
+                    '{twitter}'
+                    );
+        """.format(num=number, position=position, name=name, club=club, age=age, height=height, weight=weight, country=country, active=active, twitter=twitter)
+        cur.execute(insert_data)
 
-
-# active players only
+# inactive players only
 for n in range(37):
     print >>sys.stderr, '[{time}] Running page {n}...'.format(time=datetime.datetime.now(), n=n)
     url = 'http://www.mlssoccer.com/players?page={num}&field_player_club_nid=All&tid_2=198&title='.format(num=n)
 
-    page = urllib2.urlopen(url)
+    page = opener.open(url).read()
     soup = BeautifulSoup(page)
 
     numbers = soup.findAll('td', {'class': 'views-field views-field-field-player-jersey-no-value'})
@@ -84,20 +138,61 @@ for n in range(37):
     assert len(numbers) == len(positions) == len(names) == len(clubs) == len(ages) == len(heights) == len(weights) == len(countries) == len(actives) == len(twitters)
 
     for c in range(len(countries)):
-        number = str(numbers[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        position = str(positions[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        name = str(names[c]).split('</a>')[0].split('>')[-1].lstrip().rstrip()
-        club = str(clubs[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        age = str(ages[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        height = str(heights[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        weight = str(weights[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        country = str(countries[c]).split('>')[1].split('<')[0].lstrip().rstrip()
-        active = str(actives[c]).split('>')[1].split('<')[0].lstrip().rstrip()
+        number = unidecode(unicode(str(numbers[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if number == '':
+            number = 'Null'
+        position = unidecode(unicode(str(positions[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if position == '':
+            position = 'Null'
+        name = unidecode(unicode(str(names[c]).split('</a>')[0].split('>')[-1].lstrip().rstrip(), "utf-8"))
+        name = name.replace("'", "")
+        if name == '':
+            name = 'Null'
+        club = unidecode(unicode(str(clubs[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if club == '':
+            club = 'Null'
+        age = unidecode(unicode(str(ages[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if age == '':
+            age = 'Null'
+        height = unidecode(unicode(str(heights[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        height = height[:-3].replace("'", "''").replace("\\", "")
+        if height == '':
+            height = 'Null'
+        weight = unidecode(unicode(str(weights[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if weight == '':
+            weight = 'Null'
+        country = unidecode(unicode(str(countries[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        if country == '':
+            country = 'Null'
+        active = unidecode(unicode(str(actives[c]).split('>')[1].split('<')[0].lstrip().rstrip(), "utf-8"))
+        try:
+            twitter = str(twitters[c]).split('twitter.com/')[1].split('" target')[0].lstrip().rstrip()
+        except IndexError:
+            twitter = 'No Twitter'
         try:
             twitter = str(twitters[c]).split('twitter.com/')[1].split('" target')[0].lstrip().rstrip()
         except IndexError:
             twitter = 'No Twitter'
 
-        f.write(number + ',' + position + ',' + name + ',' + club + ',' + age + ',' + height + ',' + weight + ',' + country + ',' + active + ',' + twitter + '\n')
+        insert_data = """
+            insert into players
+                (number, position, name, club, age, height, weight, country, active, twitter)
+                values (
+                    {num},
+                    '{position}',
+                    '{name}',
+                    '{club}',
+                    {age},
+                    '{height}',
+                    {weight},
+                    '{country}',
+                    '{active}',
+                    '{twitter}'
+                    );
+        """.format(num=number, position=position, name=name, club=club, age=age, height=height, weight=weight, country=country, active=active, twitter=twitter)
+        cur.execute(insert_data)
 
-f.close()
+
+db.commit()
+
+cur.close()
