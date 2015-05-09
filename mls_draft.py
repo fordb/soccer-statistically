47e558531281bf78ae7dea2bd55fe8b1954c06e8
Ford Bohrmann <ford@simulmedia.com>
2015-05-08 21:55:43 -0400

    scrapes all years and rounds and loads into db

--- mls_draft.py
+++ mls_draft.py
@@ -1,15 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib2
 import csv
-import io
 import MySQLdb
-
+import re
 
 db = MySQLdb.connect(host="localhost", user="root", db="soccer_stat")
 cur = db.cursor()
 
-
-header = {'User-Agent': 'Mozilla/5.0'} #Needed to prevent 403 error on Wikipedia
+header = {'User-Agent': 'Mozilla/5.0'}
 
 data = []
 
@@ -26,35 +24,61 @@ create_table = """
 """
 cur.execute(create_table)
 
-for year in range(2000,2016):
-    pick = 1
-    round = 1
+
+for year in range(2000, 2016):
     wiki = "http://en.wikipedia.org/wiki/{year}_MLS_SuperDraft".format(year=year)
-    req = urllib2.Request(wiki,headers=header)
+    req = urllib2.Request(wiki, headers=header)
     page = urllib2.urlopen(req)
     soup = BeautifulSoup(page)
-    table = soup.find("table", { "class" : "wikitable sortable" })
-    if not table:
-        table = soup.find("table", { "class" : "wikitable sortable sortable" })
+    table = soup.findAll("table", { "class" : "wikitable sortable sortable" })
+    table += soup.findAll("table", { "class" : "wikitable sortable" })
 
-    for row in table.findAll("tr"):
-        cells = row.findAll("td")
-        #For each "tr", assign each "td" to a variable.
-        if len(cells) == 4:
-            team = cells[0].find(text=True).encode('ascii', 'ignore')
-            player = max(cells[1].findAll(text=True), key=len).encode('ascii', 'ignore')
-            position = cells[2].find(text=True).encode('ascii', 'ignore')
-            affiliation = cells[3].find(text=True).encode('ascii', 'ignore')
-            affiliation = affiliation.replace("'", "")
-            insert_data = """
-                insert ignore into draft
-                  (year, round, pick, club, name, position, affiliation)
-                  values ({y}, {r}, {pick}, '{c}', '{n}', '{p}', '{a}');
-            """.format(y=year, r=round, pick=pick, c=team, n=player, p=position, a=affiliation)
-            cur.execute(insert_data)
-            pick +=1
-            
-            data.append([year, pick, round, team, player, position, affiliation])
+    rounds = len(table)
+    if year in [2010, 2011]:
+        rounds = len(table)-1
+        
+    for r in range(0, rounds):
+        pick = 0
+        for row in table[r].findAll("tr"):
+            cells = row.findAll("td")
+            # For each "tr", assign each "td" to a variable.
+            length = len(cells)
+            num = length - 4
+            player = ""
+            try:
+                player = max(cells[num+1].findAll(text=True), key=len).encode(
+                    'ascii', 'ignore')
+            except:
+                IndexError
+            if player == "PASS":
+                team = cells[num].find(text=True).encode('ascii', 'ignore')
+                player = max(cells[num+1].findAll(text=True), key=len).encode(
+                    'ascii', 'ignore')
+                insert_data = """
+                    insert ignore into draft
+                      (year, round, pick, club, name, position, affiliation)
+                      values ({y}, {r}, {pick}, '{c}', NULL, NULL, NULL);
+                """.format(y=year, r=r+1, pick=pick+1, c=team, n=player)
+                cur.execute(insert_data)
+                data.append([year, pick+1, r+1, team, player, None, None])
+                pick += 1
+            elif len(cells) in [4, 5]:
+                team = cells[num].find(text=True).encode('ascii', 'ignore')
+                player = max(cells[num+1].findAll(text=True), key=len).encode(
+                    'ascii', 'ignore')
+                player = player.replace("'", "")
+                position = cells[num+2].find(text=True).encode('ascii', 'ignore')
+                affiliation = cells[num+3].find(text=True).encode('ascii', 'ignore')
+                affiliation = affiliation.replace("'", "")
+                insert_data = """
+                    insert ignore into draft
+                      (year, round, pick, club, name, position, affiliation)
+                      values ({y}, {r}, {pick}, '{c}', '{n}', '{p}', '{a}');
+                """.format(y=year, r=r+1, pick=pick+1, c=team, n=player,
+                           p=position, a=affiliation)
+                cur.execute(insert_data)
+                data.append([year, pick+1, r+1, team, player, position, affiliation])
+                pick += 1
     print year
            
 db.commit()
